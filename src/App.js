6e03be35e6e5544a8d5c4f9008bcf83570c5a420
Micayla <70994414+xmicayla@users.noreply.github.com>
2021-01-07 21:18:29 -0700

    completed basic html and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import './App.css';
+import Weather from "./Weather.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Hello World</h1>
-      </header>
-    </div>
+   <Weather />
   );
 }
 
---
+++ src/Weather.js
@@ -0,0 +1,72 @@
+import React from "react";
+import './Weather.css'
+
+export default function Weather() {
+return (
+    <div className="weather">
+        <form className="search-form">
+            <div className="row">
+                <div className="col-5">
+                    <input 
+                        type="search" 
+                        placeholder="Search for a city.."
+                        autofocus="on"
+                        className="form-control"
+                    />
+                </div>
+                 <div className="col-3">
+                    <input 
+                        value="Search" 
+                        type="submit"
+                        className="form-control"
+                    />
+                </div>   
+                <div className="col-4">
+                     <input 
+                        value="Current City"
+                        type="submit"
+                        className="form-control"
+                    />
+                </div>
+            </div>
+        </form>
+        <div className="row">
+            <div className="location col-7">
+                <h2 className="city">
+                    <strong>Vancouver</strong>
+                </h2> 
+                <h4 className="description">
+                    Cloudy
+                </h4>
+            </div>
+            
+            <div className="current-time col-5">
+                <p>
+                    14:26
+                </p>
+            </div> 
+        </div>
+    <div className="row">
+       <div className="current-temperature col-6"> 
+            <img src=""/>☁️
+            <strong>12</strong><span>ºC</span>
+            
+        </div>
+        <div className="current-specifications col-6">
+        <ul>
+            <li>
+                Feels like: <span>11</span>ºC
+            </li>
+            <li>
+                Humidity: <span>40</span>%
+            </li>
+            <li>
+                Wind: <span>0</span> m/s
+            </li>
+        </ul>
+        </div>
+    </div>
+    <div class="row forecast"></div>
+</div>
+)
+}
